b8f7c4a1b66e03d29492f0ef5d92df85593efbe0
Edrie Isaac Cruz <eddsycruz@gmail.com>
2023-02-22 14:42:23 +0800

    added video and changed title with image

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -9,7 +9,17 @@ const Main = () => {
         
           <div className="Main">
             <div className="Main-Content">
-              here goes the video
+            <video 
+              playsinline="playsinline" 
+              autoplay="autoplay" 
+              muted="muted" 
+              loop="loop"
+              className='Main-Video'>
+              <source 
+                src="https://firebasestorage.googleapis.com/v0/b/kaplet-f82b1.appspot.com/o/Niki_1.mp4?alt=media&token=5db21b50-fd47-4cb9-aa3c-1b57e69b3cda" 
+                type="video/mp4" />
+            </video>
+
             </div>
           </div>
     </>
